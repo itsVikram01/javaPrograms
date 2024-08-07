b35c32fea4f779f5e46ef8264c0e8468c2e515fa
VIKRAM KUMAR <121459513+itsVikram01@users.noreply.github.com>
2024-08-07 13:32:44 +0530

    Delete zavaprogram directory

--- zavaprogram/Main.java
+++
@@ -1,8 +0,0 @@
-package java_codes.zavaprogram;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        Circle circle = new Circle(5);
-        System.out.println("Circle Area: " + circle.calculateArea());
-    }
-}
